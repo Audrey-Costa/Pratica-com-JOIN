2a44aa57225e4dd8215ae0a70b9cd5d213c356c0
Audrey Costa <audreycota@nano.ufrj.br>
2022-07-28 18:21:17 -0300

    GROUP BY - Exercícios FInalizados

--- GROUPBY.sql
+++ GROUPBY.sql
@@ -1,3 +1,14 @@
 -- Exercício 1
-SELECT COUNT("endDate") FROM experiences;
+SELECT COUNT("endDate") AS "currentExperiences" FROM experiences;
 
+-- Exercício 2
+SELECT "userId" as id, COUNT("endDate") AS educations FROM educations GROUP BY "userId";
+
+-- Exercício 3
+SELECT users.name AS writer, COUNT("writerId") AS "testimonialCount" FROM testimonials
+JOIN users ON users.id = testimonials."writerId" WHERE testimonials."writerId" = 435
+GROUP BY users.name;
+
+-- Exercício 4
+SELECT MAX(salary) AS "maximumSalary", roles.name AS role FROM jobs JOIN roles ON jobs."roleId" = roles.id
+GROUP BY roles.name ORDER BY "maximumSalary" ASC;
